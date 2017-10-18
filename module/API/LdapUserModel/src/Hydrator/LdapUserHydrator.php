cb79f7812a13e2bb26d6f18694d5002892e06d90
Mike <mike@petmi627.lu>
2017-10-18 12:09:58 +0200

    Added Hydrator to Ldap User Model

---
+++ module/API/LdapUserModel/src/Hydrator/LdapUserHydrator.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * @author: aa43681 - Mike Peters <mike@petmi627.lu>
+ * @datetime: 18/10/2017 12:08
+ */
+
+namespace API\LdapUserModel\Hydrator;
+
+
+use Zend\Hydrator\ClassMethods;
+
+class LdapUserHydrator extends ClassMethods
+{
+
+}
