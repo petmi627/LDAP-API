ce5e4de929227deb7b36a2fa69dbb58857666efe
Mike <mike@petmi627.lu>
2017-10-19 10:51:33 +0200

    Updated User Rest Controller

--- module/API/ApiFrontend/src/Controller/UserRestController.php
+++ module/API/ApiFrontend/src/Controller/UserRestController.php
@@ -8,6 +8,7 @@
 namespace API\ApiFrontend\Controller;
 
 
+use API\LdapUserModel\Entity\LdapUserEntity;
 use API\LdapUserModel\Hydrator\LdapUserHydrator;
 use API\LdapUserModel\Repository\LdapUserRepository;
 use Zend\Mvc\Controller\AbstractRestfulController;
@@ -47,8 +48,30 @@ class UserRestController extends AbstractRestfulController
 
     public function get($id)
     {
-       $entity = $this->ldapUserRepository->getUserByCn($id);
+        $format = $this->params()->fromQuery('format', 'json');
+        $filter = $this->params()->fromQuery('filter', 'dn');
 
-       return new JsonModel($this->ldapUserHydrator->extract($entity));
+        if ($filter == 'dn') {
+            $entity = $this->ldapUserRepository->getUserByDn($id);
+            if (!$entity) {
+                return new JsonModel(['error' => 'User not found in LDAP']);
+            }
+        } else {
+            $result = $this->ldapUserRepository->searchUser($id, $filter);
+
+            if (count($result) > 0) {
+                /** @var LdapUserEntity $item */
+                foreach ($result as $item) {
+                    $entity[] = $this->ldapUserHydrator->extract($item);
+                }
+
+                return new JsonModel($entity);
+            } else if (is_null($result)) {
+                return new JsonModel(['error' => 'User not found in LDAP']);
+            }
+        }
+
+
+        return new JsonModel($this->ldapUserHydrator->extract($entity));
     }
 }
