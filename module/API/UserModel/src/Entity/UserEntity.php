40643c3855b0b4443823b36e8c46db440da17888
Mike <mike@petmi627.lu>
2017-10-19 12:53:27 +0200

    Added User Entity to User Model Module

---
+++ module/API/UserModel/src/Entity/UserEntity.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * @author: aa43681 - Mike Peters <mike@petmi627.lu>
+ * @datetime: 19/10/2017 12:48
+ */
+
+namespace API\UserModel\Entity;
+
+
+class UserEntity
+{
+    private $id;
+    private $username;
+    private $language;
+    private $created;
+    private $modified;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param mixed $username
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLanguage()
+    {
+        return $this->language;
+    }
+
+    /**
+     * @param mixed $language
+     */
+    public function setLanguage($language)
+    {
+        if (!in_array($language, ["en","de","lb","fr"])) {
+            $language = "en";
+        }
+
+        $this->language = $language;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    /**
+     * @param mixed $created
+     */
+    public function setCreated(\DateTime $created)
+    {
+        $this->created = $created;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getModified()
+    {
+        return $this->modified;
+    }
+
+    /**
+     * @param mixed $modified
+     */
+    public function setModified(\DateTime $modified)
+    {
+        $this->modified = $modified;
+    }
+
+
+}
