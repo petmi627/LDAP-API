e28b07af7421fb214207c733e5a591085b76782d
Mike <mike@petmi627.lu>
2017-10-21 19:19:43 +0200

    Added Ldap Group Hydrator to Ldap Group Model Module

---
+++ module/API/LdapGroupModel/src/Hydrator/LdapGroupHydrator.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * @author: aa43681 - Mike Peters <mike@petmi627.lu>
+ * @datetime: 21/10/2017 19:19
+ */
+
+namespace API\LdapGroupModel\Hydrator;
+
+
+use Zend\Hydrator\ClassMethods;
+
+class LdapGroupHydrator extends ClassMethods
+{
+
+}
